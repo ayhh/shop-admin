6fec156e0b636ed741f5707f437ca8d90b369478
zhengyihuang <2358559937@qq.com>
2021-08-03 14:06:53 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+
+module.exports={
+  lintOnSave: false,
+  publicPath:'./',
+  // devServer:{
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://121.5.235.28:8888",
+  //       // pathRewrite: {"^/api" : ""}
+  //     }
+  //   }
+  // }
+}
